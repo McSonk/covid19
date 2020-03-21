af877d940277ffb82f822ddb3c78ef24a6ecb1b5
Misael Alanis <misaelarturo@gmail.com>
2020-03-21 13:04:30 -0600

    Modulates the plot function to add a new tool

--- cv19/utils/plots.py
+++ cv19/utils/plots.py
@@ -5,24 +5,33 @@ import matplotlib.dates as mdates
 DATETIME_FORMAT = '%Y-%m-%d'
 WINDOW_DAYS = 14
 
-def plot_country_cases(df, country, ld_date=None):
+def __generic__plot__(df, country, ld_date, display_label):
     plt.figure(figsize=(15, 8))
-    plt.title('Confirmed cases in %s' % country)
     plt.xlabel('Date')
-    plt.ylabel('Number of cases')
     first_detected = df[country].where(lambda x: x > 0).dropna().index[0]
-    print("First case for %s detected on %s" % (country, first_detected))
     plt.plot(df.loc[df.index > first_detected, country], label='Number of cases')
+    print("First case for %s detected on %s" % (country, first_detected))
     if ld_date is not None:
         end_date = datetime.strptime(ld_date, DATETIME_FORMAT) + timedelta(days=WINDOW_DAYS)
         end_date_str = end_date.strftime(DATETIME_FORMAT)
         print("Lockdown started on %s" % (ld_date))
-        plt.axvline(ld_date, linestyle='--', color='r', label='Lockdown started')
+        plt.axvline(ld_date, linestyle='--', color='r', label=display_label)
         plt.axvline(end_date_str, linestyle=':', color='r', label='14 days window')
         plt.legend()
     num_of_cases = df[country].dropna().iloc[-1]
     plt.xticks(rotation=80)
     print("Currently having %s confirmed cases" % f'{num_of_cases:,}')
+
+def plot_country_cases(df, country, ld_date=None):
+    __generic__plot__(df, country, ld_date, 'Number of cases')
+    plt.title('Confirmed cases in %s' % country)
+    plt.ylabel('Number of cases')
+    plt.show()
+
+def plot_country_changes(df, country, ld_date=None):
+    __generic__plot__(df, country, ld_date, 'New cases')
+    plt.title('Confirmed new cases in %s' % country)
+    plt.ylabel('Number of new cases')
     plt.show()
 
 
