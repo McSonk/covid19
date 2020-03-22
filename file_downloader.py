74e89a51cfc920ea943cc2f1aaa5b161b62838e7
Misael Alanis <misaelarturo@gmail.com>
2020-03-21 19:49:30 -0600

    Adds methods to download the deaths file

--- file_downloader.py
+++ file_downloader.py
@@ -1,11 +1,24 @@
 from urllib.request import urlretrieve
 
 CONFIRMED_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+DEATHS_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
 DIR_PATH = 'data/'
 CONFIRMED_FILENAME = 'confirmed.csv'
+DEATHS_FILENAME= 'deaths.csv'
 
-if __name__ == "__main__":
+def download_confirmed():
     print("Downloading data from url %s" % CONFIRMED_URL)
     print("Target location: %s%s" % (DIR_PATH, CONFIRMED_FILENAME))
     urlretrieve(CONFIRMED_URL, '%s%s' % (DIR_PATH, CONFIRMED_FILENAME))
     print("Done!")
+
+def download_deaths():
+    print("Downloading data from url %s" % DEATHS_URL)
+    print("Target location: %s%s" % (DIR_PATH, DEATHS_FILENAME))
+    urlretrieve(DEATHS_URL, '%s%s' % (DIR_PATH, DEATHS_FILENAME))
+    print("Done!")
+
+if __name__ == "__main__":
+    download_confirmed()
+    print("********************")
+    download_deaths()
--- file_translator.py
+++ file_translator.py
@@ -5,8 +5,15 @@ DIR_NAME = 'data/'
 held by the John Hopkings University'''
 CONF_FILE_NAME = 'confirmed.csv'
 
+'''Filename of the confirmed deaths CSV. This file is obtained from data
+held by the John Hopkings University'''
+DEATH_FILE_NAME = 'deaths.csv'
+
 '''Where the result CSV will be stored'''
 CONF_TARGET_FILE = 'conf_per_day.csv'
+
+'''Where the result CSV will be stored'''
+DEATH_TARGET_FILE = 'deaths_per_day.csv'
 
 '''Columns not to be used for further analysis
 Province/State: The state within the country where the reports comes from
@@ -22,12 +29,12 @@ ORIGINAL_COUNTRY_COL_NAME = 'Country/Region'
 COUNTRY_COL_NAME = 'Country'
 
 
-if __name__ == "__main__":
+def parse_file(file_name, target):
     #Dictionary to make the replacement of the column names
     COLS_REPLACE = {ORIGINAL_COUNTRY_COL_NAME: COUNTRY_COL_NAME}
     #Read the CSV.
-    print('Reading file "%s%s"...' % (DIR_NAME, CONF_FILE_NAME))
-    data = pd.read_csv('%s%s' % (DIR_NAME, CONF_FILE_NAME))
+    print('Reading file "%s%s"...' % (DIR_NAME, file_name))
+    data = pd.read_csv('%s%s' % (DIR_NAME, file_name))
     print("done!")
     data.drop(UNUSED_COLS, 1, inplace=True)
     data.rename(columns=COLS_REPLACE, inplace=True)
@@ -41,6 +48,11 @@ if __name__ == "__main__":
     #Rename index col, and make pandas interprete it as a datetime object
     by_date.index.names = ['Date']
     by_date.index = pd.to_datetime(by_date.index)
-    print("Writing to file '%s%s'" % (DIR_NAME, CONF_TARGET_FILE))
-    by_date.to_csv('%s%s' % (DIR_NAME, CONF_TARGET_FILE))
+    print("Writing to file '%s%s'" % (DIR_NAME, target))
+    by_date.to_csv('%s%s' % (DIR_NAME, target))
     print("Done! Have a nice day!")
+
+if __name__ == "__main__":
+    parse_file(CONF_FILE_NAME, CONF_TARGET_FILE)
+    print("***********************")
+    parse_file(DEATH_FILE_NAME, DEATH_TARGET_FILE)
