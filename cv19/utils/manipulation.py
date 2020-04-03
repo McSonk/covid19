38d495f273aa843460059e16877e2278356b36c8
Misael Alanis <misaelarturo@gmail.com>
2020-04-02 21:33:05 -0600

    Add function to find  what countries were infected

--- cv19/utils/manipulation.py
+++ cv19/utils/manipulation.py
@@ -25,3 +25,17 @@ def build_first_n_df(df: DataFrame, countries: list, n: int, target: DataFrame, 
         else:
             target[country], date = first_n_cases(df, country, n, first_dates[country])
     return dates
+
+def which_countries_before(df: DataFrame, search_country: str):
+    aux = df.drop('the world', axis=1)
+    infected_countries = []
+    for i in range(0, len(aux)):
+        c = aux.iloc[i]
+        c = c[c > 0]
+        lote = c.index
+        for country in lote:
+            if country == search_country:
+                return infected_countries
+            if country not in infected_countries:
+                infected_countries.append(country)
+    return None
